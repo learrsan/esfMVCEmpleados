3ac751a901901ad0975d30f57df8b09861f75101
learrsan <gaslas@hotmail.com>
2015-03-05 19:33:33 +0100

    T5/3/15

---
+++ Empleados/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using Empleados.Models;
+
+namespace Empleados.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var db = new EmpleadosEntities();
+            {
+
+                return View(db.Empleados.ToList());
+            }
+
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new Models.Empleados());
+        }
+
+        [HttpPost]
+
+        public ActionResult Alta(Models.Empleados model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new EmpleadosEntities())
+                {
+                    db.Empleados.Add(model);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Buscar()
+        {
+            var bus = Request.Form["busqueda"];
+            var db = new EmpleadosEntities();
+            var al = db.Empleados.Where(o => o.apellidos.Contains(bus));
+
+            return View(al);
+
+        }
+    }
+}
---
+++ Empleados/Models/BuscadosEmpleados.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Html;
+
+namespace Empleados.Models
+{
+    public class BuscadorEmpleados
+    {
+
+        EmpleadosEntities db = new EmpleadosEntities();
+
+        public List<Empleados> GetPorApellidos(String apellidos)
+        {
+            var datos = from o in db.Empleados
+                        where o.apellidos == apellidos
+                        select o;
+
+            return datos.ToList();
+        }
+
+
+        public List<Empleados> GetPorApellidosLambda(String apellidos)
+        {
+
+            var datos = db.Empleados.Where(o => o.apellidos == apellidos && o.nombre == "Luis");
+            return datos.ToList();
+        }
+    }
+}
